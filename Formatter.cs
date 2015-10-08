1f5a1c0abb84728d260670ded71a42727accdde9
SteGriff <github@stegriff.co.uk>
2015-10-08 10:13:47 +0100

    Refactored business into separate class

---
+++ Formatter.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dnformat
+{
+    public static class Formatter
+    {
+
+        public static void FormatOutput(string format, string paramCollector)
+        {
+            //Format stage
+
+            IList<object> paramObjects = new List<object>();
+
+            string[] typesAndParams = paramCollector.Split(':', ',');
+
+            int j = 0;
+            Type lastContentType = typeof(object);
+
+            foreach (string tp in typesAndParams)
+            {
+
+                //If even (0, 2...) then its a type
+                if (j % 2 == 0)
+                {
+                    //Get the type
+                    try
+                    {
+                        string typeName = realTypeNameOf(tp);
+                        lastContentType = Type.GetType(typeName);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Not a type: {0}", tp);
+                    }
+                }
+                else
+                {
+                    //Do the cast
+                    try
+                    {
+                        var thing = Convert.ChangeType(tp, lastContentType);
+                        paramObjects.Add(thing);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Can't format {0} as {1}", tp, lastContentType);
+                    }
+                }
+
+                j++;
+            }
+
+            try
+            {
+                string formatted = string.Format(format, paramObjects.ToArray());
+                Console.WriteLine(formatted);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("{0}: {1}", ex.GetType().ToString(), ex.Message));
+                if (ex.GetType() == typeof (FormatException))
+                {
+                    Console.WriteLine("  Did you mismatch the number of params and placeholders?");
+                    Console.WriteLine("  Did you separate params with commas? E.g. int:3,string:hello");
+                }
+                
+            }
+        }
+
+        private static string realTypeNameOf(string typeName)
+        {
+            switch (typeName)
+            {
+                case "object":
+                    return "System.Object";
+
+                case "string":
+                    return "System.String";
+
+                case "bool":
+                    return "System.Boolean";
+
+                case "byte":
+                    return "System.Byte";
+
+                case "sbyte":
+                    return "System.SByte";
+
+                case "short":
+                    return "System.Int16";
+
+                case "ushort":
+                    return "System.UInt16";
+
+                case "int":
+                    return "System.Int32";
+
+                case "uint":
+                    return "System.UInt32";
+
+                case "long":
+                    return "System.Int64";
+
+                case "ulong":
+                    return "System.UInt64";
+
+                case "float":
+                    return "System.Single";
+
+                case "double":
+                    return "System.Double";
+
+                case "decimal":
+                    return "System.Decimal";
+
+                case "char":
+                    return "System.Char";
+
+                default:
+                    return typeName;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace dnformat
 {
@@ -20,15 +18,7 @@ namespace dnformat
             {
                 if (helpFlags.Contains(args[0]))
                 {
-                    string usage =
-@"Usage:
-    dnformat format type1:param1,type2:param2...
-Examples:
-    dnformat ""hello {0}"" string:world
-    dnformat ""{0:P} glue"" float:0.05
-    dnformat ""{0:X} {0:X08}"" int:37295";
-
-                    Console.WriteLine(usage);
+                    OutputHelp();
                     return;
                 }
 
@@ -40,8 +30,8 @@ Examples:
                 Console.Write("Enter format string: ");
                 format = Console.ReadLine();
 
-                Console.WriteLine("Enter params with type (enter blank to finish)");
-                Console.WriteLine("E.g. int 50");
+                Console.WriteLine("Enter params as type:value (enter blank to finish)");
+                Console.WriteLine("E.g. int:50");
 
                 int i = 0;
                 string thisContent = "";
@@ -55,115 +45,26 @@ Examples:
                         paramCollector += thisContent;
                         i++;
                     }
-                } while (thisContent != "");
-            }
-
-            //Format stage
-
-            IList<object> paramObjects = new List<object>();
-
-            string[] typesAndParams = paramCollector.Split(':', ',');
-
-            int j = 0;
-            Type lastContentType = typeof(object);
-
-            foreach(string tp in typesAndParams)
-            {
-                
-                //If even (0, 2...) then its a type
-                if (j % 2 == 0)
-                {
-                    //Get the type
-                    try
-                    {
-                        string typeName = realTypeNameOf(tp);
-                        lastContentType = Type.GetType(typeName);
-                    }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("Not a type: {0}", tp);
-                    }
                 }
-                else
-                {
-                    //Do the cast
-                    try
-                    {
-                        var thing = Convert.ChangeType(tp, lastContentType);
-                        paramObjects.Add(thing);
-                    }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("Can't format {0} as {1}", tp, lastContentType);
-                    }
-                }
-
-                j++;
+                while (thisContent != "");
             }
 
-            try
-            {
-                string formatted = string.Format(format, paramObjects.ToArray());
-                Console.WriteLine(formatted);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(string.Format("{0}: {1}", ex.GetType().ToString(), ex.Message));
-            }
+            Formatter.FormatOutput(format, paramCollector);
+
         }
 
-        static string realTypeNameOf(string typeName)
+        private static void OutputHelp()
         {
-            switch (typeName)
-            {
-                case "object":
-                    return "System.Object";
+            string usage =
+@"Usage:
+    dnformat format type1:param1,type2:param2...
+Examples:
+    dnformat ""hello {0}"" string:world
+    dnformat ""{0:P} glue"" float:0.05
+    dnformat ""{0:X} {0:X08}"" int:37295";
 
-                case "string":
-                    return "System.String";
-
-                case "bool":
-                    return "System.Boolean";
-
-                case "byte":
-                    return "System.Byte";
-
-                case "sbyte":
-                    return "System.SByte";
-
-                case "short":
-                    return "System.Int16";
-
-                case "ushort":
-                    return "System.UInt16";
-
-                case "int":
-                    return "System.Int32";
-
-                case "uint":
-                    return "System.UInt32";
-
-                case "long":
-                    return "System.Int64";
-
-                case "ulong":
-                    return "System.UInt64";
-
-                case "float":
-                    return "System.Single";
-
-                case "double":
-                    return "System.Double";
-
-                case "decimal":
-                    return "System.Decimal";
-
-                case "char":
-                    return "System.Char";
-
-                default:
-                    return typeName;
-            }
+            Console.WriteLine(usage);
         }
+
     }
 }
